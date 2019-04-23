02efe8244a0236f1689875c90697831720ce36e8
Arvind Thangamani <dnivra26@gmail.com>
2019-04-23 17:04:15 +0530

    page update - Tue Apr 23 17:04:15 IST 2019

---
+++ precache-manifest.c1a0d631b169719c9bb919b504e14e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab09c9e4ba1fb7151858",
+    "url": "/snake_react/static/js/runtime~main.ed6f5f00.js"
+  },
+  {
+    "revision": "7e2b8912a025e98f6eff",
+    "url": "/snake_react/static/js/main.24c39d06.chunk.js"
+  },
+  {
+    "revision": "e554c4546e0c7a2d6776",
+    "url": "/snake_react/static/js/2.2b1fa201.chunk.js"
+  },
+  {
+    "revision": "7e2b8912a025e98f6eff",
+    "url": "/snake_react/static/css/main.587437df.chunk.css"
+  },
+  {
+    "revision": "f4c66362d31da30296031dd4c6a66931",
+    "url": "/snake_react/index.html"
+  }
+];
